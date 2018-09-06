75a888b4332442f76d2574c8e0b64cd4f18e00f7
Thomas Krzero <thomas.kovatchitch@gmail.com>
2018-09-06 22:31:20 +0200

    WIP

--- hello-world/env-init.sh
+++ hello-world/env-init.sh
@@ -1,2 +1,3 @@
+git clone https://github.com/WTFKr0/katacoda-scenarios.git
 docker swarm init
-docker stack deploy -c swarm-system.yaml swarm-system
+docker stack deploy -c katacoda-scenarios/hello-world/swarm-system.yaml swarm-system
