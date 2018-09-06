09dd9242234b280f34d51b5acd1c18fe3e502611
Thomas Krzero <thomas.kovatchitch@gmail.com>
2018-09-06 21:59:38 +0200

    WIP

--- hello-world/env-init.sh
+++ hello-world/env-init.sh
@@ -1,1 +1,11 @@
 docker swarm init
+
+cat <<EOF | docker stack deploy -c - swarm-system
+version: "3.5"
+services:
+  traefik:
+    image: traefik:tetedemoine
+    ports:
+      - 80:80
+      - 443:443
+EOF
